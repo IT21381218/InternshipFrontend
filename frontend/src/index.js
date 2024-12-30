cdecff663ae44db1bcd60225aaecfda0d37d99c6
poornaka <99286090+IT21381218@users.noreply.github.com>
2024-12-30 20:06:06 +0530

    frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // The default CSS file for global styles
+import App from './App'; // Import the main App component
+
+document.title = "ZADE";
+// Create a root and render the App component to the DOM
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
