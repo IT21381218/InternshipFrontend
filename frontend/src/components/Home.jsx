e941d5810b78e1f1b9562478f0402b09a9efb7e4
poornaka <99286090+IT21381218@users.noreply.github.com>
2024-12-31 23:08:43 +0530

    d

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -74,7 +74,7 @@ const Home = () => {
             <iframe
               width="100%"
               height="315"
-              src="https://res.cloudinary.com/dwcxwpn7q/video/upload/v1734544185/zade/x_c6e9br.mp4"
+              src="https://www.youtube.com/embed/PTOAoH_MMwY"
               title="YouTube video player"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
